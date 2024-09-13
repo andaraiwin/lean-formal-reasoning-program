b7fd3a630baa6de1f04bf2852f69abaf037782b3
AndaRaiwin <andaraiwin@gmail.com>
2024-09-14 01:19:27 +0700

    finished Trie's exercise

--- Frap/Exercises/Trie.lean
+++ Frap/Exercises/Trie.lean
@@ -8,14 +8,25 @@ open Trie
 exercise (1-star)
 -/
 theorem look_leaf' α (d : α) j : look d j leaf = d := by
-  sorry
+  unfold look
+  rfl
 
 /-
 exercise (2-star)
 This is a rather simple induction.
 -/
+local macro "simp'" : tactic =>
+  `(tactic| (unfold ins look; simp [*] at *))
+
 theorem look_ins_same' {α} d k (v : α) : ∀ t, look d k (ins d k v t) = v := by
-  sorry
+  intro t
+  induction k generalizing t with
+  | xI k ih =>
+    cases t <;> simp'
+  | xO k ih =>
+    cases t <;> simp'
+  | xH =>
+    cases t <;> simp'
 
 /-
 exercise (2-star)
@@ -23,7 +34,11 @@ Use `pos2nat2pos` and `nat2pos2nat` lemmas to prove the following injection prop
 -/
 
 theorem pos2nat_injective' p q : pos2nat p = pos2nat q → p = q := by
-  sorry
+  intro h
+  rw [←pos2nat2pos p, ←pos2nat2pos q]
+  rw [←h]
 
 theorem nat2pos_injective' i j : nat2pos i = nat2pos j → i = j := by
-  sorry
+  intro h
+  rw [←nat2pos2nat i, ←nat2pos2nat j]
+  rw [←h]
